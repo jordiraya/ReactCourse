f07a743775c0ae06b765a53e7fcb38b6c0c8b9e2
Jordi Raya Lapuente <raya.jordi@gmail.com>
2019-02-12 17:45:23 +0100

    Section 2.36: understanding the children property

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,19 +11,46 @@ class App extends Component {
     ]
   };
   
-  switchNameHandler = () => {
-    console.log('Was clicked');
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked');
+    // DON'T DO THIS: this.state.persons[0].name = 'Pepe'
+    // don't mutate the state directly, use setState
+    this.setState( {
+      persons: [
+        { name: newName, age: "28" },
+        { name: "Pipo", age: "29" },
+        { name: "Pepo", age: "30" }      
+      ]
+    });
   }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: "Pepe", age: "28" },
+        { name: event.target.value, age: "29" },
+        { name: "Pepo", age: "30" }      
+      ]
+    });    
+  };   
 
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Maximilian')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Maximilianus')}
+          changed={this.nameChangedHandler}>My hobbies: racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
   }
